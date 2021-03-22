9ad6ece78a34eb06a81c74a8b9da64c9f6232317
Casey Lockwood <kclockwood32@gmail.com>
2021-03-22 15:38:04 -0600

    Further updates to js structure

--- characters/main.js
+++ characters/main.js
@@ -5,8 +5,9 @@ const mainContent = document.querySelector('#main')
 people.forEach(person => {
     const charFigure = document.createElement('figure')
     const charImg = document.createElement('img')
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/1.jpg`
     const charCaption = document.createElement('figcaption')
-    charImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+
     charCaption.textContent = person.name
 
     charFigure.appendChild(charImg)
