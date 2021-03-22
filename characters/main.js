53c914a7e45687e3d55796935a66c42e165758ea
Casey Lockwood <kclockwood32@gmail.com>
2021-03-22 15:20:39 -0600

    set up basic js code variables

--- characters/main.js
+++ characters/main.js
@@ -1,1 +1,15 @@
-console.log("Welcome")
+import { people } from '../data/people.js'
+
+const mainContent = document.querySelector('#main')
+
+people.forEach(person => {
+    const charFigure = document.createElement('figure')
+    const charImg = document.createElement('img')
+    const charCaption = document.createElement('figcaption')
+    charImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+    charCaption.textContent = person.name
+
+    charFigure.appendChild(charImg)
+    charFigure.appendChild(charCaption)
+    mainContent.appendChild(charFigure)
+})
