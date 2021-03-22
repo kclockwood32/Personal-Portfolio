ea7c6df0330e7ece1e4dc6706081d7a88d4dcec1
Casey Lockwood <kclockwood32@gmail.com>
2021-03-22 16:29:13 -0600

    Added sort characters by gender functionality

--- characters/main.js
+++ characters/main.js
@@ -2,10 +2,34 @@ import { people } from '../data/people.js'
 
 const mainContent = document.querySelector('#main')
 
-people.forEach(person => {
+const mainHeader = document.createElement('header')
+
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
+maleButton.addEventListener('click', () => {
+    populateDOM(maleCharacters)
+})
+
+mainHeader.appendChild(maleButton)
+document.body.insertBefore(mainHeader, mainContent)
+
+
+const maleCharacters = people.filter(person => person.gender === 'male')
+const femaleCharacters = people.filter(person => person.gender === 'female')
+const otherCharacters = people.filter(person => {
+    if (person.gender === 'n/a' || person.gender === 'none') {
+        return person
+    }
+})
+
+console.log(otherCharacters)
+
+function populateDOM(characters) {
+characters.forEach(person => {
     const charFigure = document.createElement('figure')
     const charImg = document.createElement('img')
-    charImg.src = `https://starwars-visualguide.com/assets/img/characters/1.jpg`
+    let charNum = getLastNumber(person.url)
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
     const charCaption = document.createElement('figcaption')
 
     charCaption.textContent = person.name
@@ -14,3 +38,13 @@ people.forEach(person => {
     charFigure.appendChild(charCaption)
     mainContent.appendChild(charFigure)
 })
+}
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if (url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
