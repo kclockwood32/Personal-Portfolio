99641540ee197bd4d27561f7cd95fa9d8e0b0966
Casey Lockwood <kclockwood32@gmail.com>
2021-04-04 13:36:32 -0600

    Added folder structure, started basic js code

---
+++ pokemon/pokecode.js
@@ -0,0 +1,14 @@
+const pokeGrid = document.querySelector('.pokeGrid')
+const loadButton = document.querySelector('.loadPokemon')
+
+loadButton.addEventListener('click', () => {
+    loadPage()
+})
+
+async function getAPIData(url) {
+    try {
+
+    } catch (error) {
+    console.log(error)
+    }
+}
