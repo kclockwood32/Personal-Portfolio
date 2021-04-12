1b1696f1e3fee65cc0b745c5b3ac118a2e140d1c
Casey Lockwood <kclockwood32@gmail.com>
2021-04-12 15:35:41 -0600

    Got rid of my old code, set up new images and code

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,19 +1,53 @@
-// const pokeGrid = document.querySelector('.pokeGrid')
-// const loadButton = document.querySelector('.loadPokemon')
+const pokeGrid = document.querySelector('.pokeGrid')
+const loadButton = document.querySelector('.loadPokemon')
 
-// loadButton.addEventListener('click', () => {
-//     loadPage()
-// })
+loadButton.addEventListener('click', () => {
+    loadPage()
+})
 
-// async function getAPIData(url) {
-//     try {
+async function getAPIData(url) {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    } catch (error) {
+      console.log(error)
+    }
+}
 
-//     } catch (error) {
-//     console.log(error)
-//     }
-// }
+function loadPage() {
+  getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=26`).then(
+    (data) => {
+        for (const singlePokemon of data.results) {
+        console.log(data)
+        }
+    }
+  )
+}
 
-var card = document.querySelector('.card');
-card.addEventListener( 'click', function() {
-  card.classList.toggle('is-flipped');
-});
+function populatePokeCard(singlePokemon) {
+  let pokeScene = document.createElement('div')
+  pokeScene.className = 'scene'
+  let pokeCard = document.createElement('div')
+  pokeCard.className = 'card'
+  pokeCard.appendChild(populateCardFront(singlePokemon))
+  pokeScene.appendChild(pokeCard)
+  pokeGrid.appendChild(pokeScene)
+  console.log(singlePokemon)
+}
+
+function populateCardFront(pokemon) {
+  let pokeFront = document.createElement('div')
+  pokeFront.className = 'card__face card__face--front'
+  let frontLabel = document.createElement('p')
+  frontLabel.textContent = pokemon.name
+  
+  pokeFront.appendChild(frontLabel)
+  return pokeFront
+
+}
+
+// var card = document.querySelector('.card');
+// card.addEventListener( 'click', function() {
+//   card.classList.toggle('is-flipped');
+// });
